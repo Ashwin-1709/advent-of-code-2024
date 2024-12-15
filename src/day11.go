b2acae962709243e479581acce61a1864b2f0bb0
Ashwin Pugalia <ashwinpugalia1@gmail.com>
2024-12-15 11:45:46 +0530

    refactor day11

--- src/day11.go
+++ src/day11.go
@@ -31,30 +31,30 @@ type state struct {
 var dp = make(map[state]int)
 
 func f(stone, turns int) int {
-  cur := state{stone, turns}
-  val, cached := dp[cur]
-  if cached {
-    return val
-  }
-  if turns == 0 {
-    dp[cur] = 1
-    return 1
-  }
+	cur := state{stone, turns}
+	val, cached := dp[cur]
+	if cached {
+		return val
+	}
+	if turns == 0 {
+		dp[cur] = 1
+		return 1
+	}
 
-  if stone == 0 {
-    dp[cur] = f(1, turns - 1)
-  } else {
-    str := strconv.Itoa(stone)
-    if len(str) % 2 != 0 {
-      dp[cur] = f(2024 * stone, turns - 1)
-    } else {
-      l, r := str[:len(str)/2], str[len(str)/2:]
-      lv, _ := strconv.Atoi(l)
-      rv, _ := strconv.Atoi(r)
-      dp[cur] = f(lv, turns - 1) + f(rv, turns - 1)
-    }
-  }
-  return dp[cur]
+	if stone == 0 {
+		dp[cur] = f(1, turns-1)
+	} else {
+		str := strconv.Itoa(stone)
+		if len(str)%2 != 0 {
+			dp[cur] = f(2024*stone, turns-1)
+		} else {
+			l, r := str[:len(str)/2], str[len(str)/2:]
+			lv, _ := strconv.Atoi(l)
+			rv, _ := strconv.Atoi(r)
+			dp[cur] = f(lv, turns-1) + f(rv, turns-1)
+		}
+	}
+	return dp[cur]
 }
 
 func simulate(stones []int, k int) []int {
@@ -84,9 +84,9 @@ func simulate(stones []int, k int) []int {
 func main() {
 	stones := parseInput()
 	ans := 0
-  for _, stone := range stones {
-    ans += f(stone, 75)
-  }
-  
-  fmt.Println(ans)
+	for _, stone := range stones {
+		ans += f(stone, 75)
+	}
+
+	fmt.Println(ans)
 }
